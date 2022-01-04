610b26aec0ea0c39a03f65ebb84f5db2f5656c2c
MOKSH <mokshteng@gmail.com>
2022-01-04 18:35:18 +0530

    Project started and dependencies installed

---
+++ src/main/java/com/project/noteappmain/NoteApp2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.noteappmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApp2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteApp2ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/noteappmain/NoteApp2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.noteappmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApp2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
